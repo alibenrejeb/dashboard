a2ce61f30426ee43cb4b681900911cd58801666e
Ali Ben Rejeb <b.rejeb.ali@gmail.com>
2017-11-26 12:47:35 +0100

    add team CRUD into ng smart table

--- src/app/pages/teams/teams.component.ts
+++ src/app/pages/teams/teams.component.ts
@@ -12,18 +12,22 @@ import { Ng2SmartTableModule, LocalDataSource } from 'ng2-smart-table';
 export class TeamsComponent implements OnInit {
 
   teams: Array<Team> = [];
+  team: Team;
   errorMessage: string;
+  statusCode: string;
 
   settings = {
     add: {
       addButtonContent: '<i class="ion-ios-plus-outline"></i>',
       createButtonContent: '<i class="ion-checkmark"></i>',
       cancelButtonContent: '<i class="ion-close"></i>',
+      confirmCreate: true,
     },
     edit: {
       editButtonContent: '<i class="ion-edit"></i>',
       saveButtonContent: '<i class="ion-checkmark"></i>',
       cancelButtonContent: '<i class="ion-close"></i>',
+      confirmSave: true,
     },
     delete: {
       deleteButtonContent: '<i class="ion-trash-a"></i>',
@@ -66,7 +70,13 @@ export class TeamsComponent implements OnInit {
   }
 
   onDeleteConfirm(event): void {
+    console.log('onDeleteConfirm');
     if (window.confirm('Are you sure you want to delete?')) {
+      let teamId = event.data['id'];
+      this.teamsService.deleteTeamById(teamId).subscribe(
+        statusCode => console.log(statusCode + " No content"),
+        errorCode => this.statusCode = errorCode
+      );
       event.confirm.resolve();
     } else {
       event.confirm.reject();
@@ -74,9 +84,33 @@ export class TeamsComponent implements OnInit {
   }
 
   onCreateConfirm(event):void {
-  } 
+    console.log('onCreateConfirm');
+    console.log(event);
+    if (window.confirm('Are you sure you want to create?')) {
+      this.team = event.newData;
+      this.teamsService.createTeam(this.team).subscribe(
+        statusCode => console.log(statusCode + " OK"),
+        errorCode => this.statusCode = errorCode
+      );
+      event.confirm.resolve(event.newData);
+    } else {
+      event.confirm.reject();
+    }
+  }
 
   onSaveConfirm(event):void {
+    console.log('onSaveConfirm');
+    //console.log(event);
+    if (window.confirm('Are you sure you want to save?')) {
+      this.team = event.newData;
+      this.teamsService.updateTeam(this.team).subscribe(
+        statusCode => console.log(statusCode + " OK"),
+        errorCode => this.statusCode = errorCode
+      );
+      event.confirm.resolve(event.newData);
+    } else {
+      event.confirm.reject();
+    }
   }
 
 }
--- src/app/services/teams/teams.service.ts
+++ src/app/services/teams/teams.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Response, Headers } from '@angular/http';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
@@ -21,6 +21,45 @@ export class TeamsService {
         return this.http.get(this.uri, {headers: headers})
             .map(res => <Team[]> res.json())
             .catch(this.handelError);
+    }
+
+    createTeam(team: Team): Observable<number> {
+        const headers = new Headers();
+        headers.append('content-type', 'application/json');
+        headers.append('Authorization', 'Bearer ' + this.authenticationService.token);
+        return this.http.post(this.uri, this.hydrate(team), {headers: headers})
+            .map(success => success.status)
+            .catch(this.handelError);
+    }
+
+    updateTeam(team: Team): Observable<number> {
+        //console.log(team);
+        //console.log("id: " + team.id);
+        const headers = new Headers();
+        headers.append('content-type', 'application/json');
+        headers.append('Authorization', 'Bearer ' + this.authenticationService.token);
+        //console.log(headers);
+        //console.log(JSON.stringify(this.hydrate(team)));
+        return this.http.put(this.uri + '/' + team.id, this.hydrate(team), {headers: headers})
+            .map(success => success.status)
+            .catch(this.handelError);
+    }
+
+    deleteTeamById(teamId: number): Observable<number> {
+        const headers = new Headers();
+        headers.append('content-type', 'application/json');
+        headers.append('Authorization', 'Bearer ' + this.authenticationService.token);
+        return this.http.delete(this.uri + "/" + teamId, {headers: headers})
+            .map(success => success.status)
+            .catch(this.handelError);
+    }
+
+    private hydrate(team: Team) {
+      return {
+        "name" : team.name,
+        "code" : team.code,
+        "shortName" : team.short_name
+      }
     }
 
     private handelError(error: Response) {
